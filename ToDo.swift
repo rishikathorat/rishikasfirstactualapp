9cdccc2f1aba890a94760317e179cba7601c7ce3
Apple <apple@Apples-MBP-30.attlocal.net>
2019-07-15 09:36:31 -0700

    Commit

---
+++ ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  rishikasfirstactualapp
+//
+//  Created by Apple on 7/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
